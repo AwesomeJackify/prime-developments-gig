3b25082efcfdd9d1b60ffbd1c30481d5521b9da0
songexile <psg3398@autuni.ac.nz>
2024-08-08 23:11:11 +1200

    Added links to each service page

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,8 +10,20 @@ const projectsCollection = defineCollection({
     floorPlan: z.string(),
   }),
 });
+
+const servicesCollection = defineCollection({
+  type: 'content', // v2.5.0 and later
+  schema: z.object({
+    name: z.string(),
+    description: z.string(),
+    featuredImage: z.string(),
+  
+  }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   'projects': projectsCollection,
+  'services': servicesCollection,
 };
---
+++ src/content/services/planning-and-design.md
@@ -0,0 +1,7 @@
+---
+name: "Residential Developments"
+description: "We are experts in developing residential properties. We have developed various townhouses around the Canterbury region. We cover the whole process for you and provide you with either predesigned drawings or we can work with you to develop the vision of your dream house.
+
+ "
+featuredImage: "demolition.jpg"
+---
