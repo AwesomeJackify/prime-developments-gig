84789b792efd2a34e005f395792d9dd89aa41098
Jack Gong <jackgong48@gmail.com>
2024-08-15 11:56:48 +1200

    ADDED: transportables

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,19 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 // 2. Define your collection(s)
 const buildsCollection = defineCollection({
-  type: 'content', // v2.5.0 and later
+  type: "content", // v2.5.0 and later
   schema: z.object({
     name: z.string(),
     description: z.string(),
     featuredImage: z.string(),
     floorPlan: z.string(),
+    gallery: z.array(z.string()).optional(),
   }),
 });
 
 const servicesCollection = defineCollection({
-  type: 'content', // v2.5.0 and later
+  type: "content", // v2.5.0 and later
   schema: z.object({
     name: z.string(),
     description: z.string(),
@@ -24,6 +25,6 @@ const servicesCollection = defineCollection({
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'builds': buildsCollection,
-  'services': servicesCollection,
+  builds: buildsCollection,
+  services: servicesCollection,
 };
